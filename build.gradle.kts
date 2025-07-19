59d741148023ac909612743b6f6a2055eebde684
bitfist <me@renelebherz.com>
2025-07-19 04:20:14 +0200

    chore: migrate to io.github.bitfist.gradle-github-support.release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
-import git.semver.plugin.changelog.ChangeLogFormatter
-import git.semver.plugin.changelog.ChangeLogTextFormatter
-import org.gradle.process.internal.ExecException
-import java.io.ByteArrayOutputStream
+import java.net.URI
 
 plugins {
     `java-library`
-    jacoco
-    `maven-publish`
-    id("com.github.jmongard.git-semver-plugin") version "0.16.1"
+    id("io.github.bitfist.gradle-github-support.release")
 }
+
+group = "io.github.bitfist"
+description = "Spring Boot starter for OS detection."
 
 //region Dependencies
 repositories {
@@ -22,6 +20,8 @@ dependencies {
 
     // Dependencies
     implementation("org.springframework.boot:spring-boot-autoconfigure")
+
+    // Test dependencies
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
@@ -31,182 +31,20 @@ dependencies {
 java {
     withSourcesJar()
     withJavadocJar()
-
     sourceCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
-
-tasks.test {
-    finalizedBy(tasks.jacocoTestReport)
-}
-
-tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-    reports {
-        csv.required = true
-    }
-}
-
-tasks.withType<Jar> {
-    manifest {
-        attributes["Implementation-Title"] = project.name
-        attributes["Implementation-Version"] = project.version
-        attributes["Automatic-Module-Name"] = project.name.replace('-', '.')
-    }
-
-    from("LICENSE") {
-        into("META-INF")
-        rename { "LICENSE_${archiveBaseName.get()}" }
-    }
-}
 //endregion
 
-//region Semantic versioning
-semver {
-    patchPattern = "\\A(fix|chore|docs|refactor|ci|build|test|deps)(?:\\([^()]+\\))?:"
 
-    val server = System.getenv("GITHUB_SERVER_URL") ?: "https://github.com"
-    val repository = System.getenv("GITHUB_REPOSITORY") ?: "bitfist/os-conditions-spring-boot-starter"
-    val repositoryUrl = "$server/$repository"
-
-    fun ChangeLogTextFormatter.appendChangeLongEntry() {
-        val longHash = hash().take(40)
-        val shortHash = longHash.take(8)
-
-        append("- [$shortHash]($repositoryUrl/commit/$longHash) ").append(scope()).appendLine(header())
-    }
-
-    changeLogFormat = ChangeLogFormatter {
-        constants.headerTexts["refactor"] = "### Refactorings \uD83D\uDD28"
-
-        appendLine(constants.header).appendLine()
-
-        withType("release") {
-            skip()
-        }
-
-        // breaking
-        withBreakingChanges {
-            appendLine(constants.breakingChange)
-            formatChanges {
-                appendChangeLongEntry()
-            }
-            appendLine()
-        }
-
-        // fix, feat, refactor
-        withType(types = arrayOf("feat", "fix", "refactor")) {
-            appendLine(constants.headerTexts[groupKey])
-            with({ constants.headerTexts.containsKey(it.scope) }) {
-                formatChanges {
-                    appendChangeLongEntry()
-                }
-            }
-            formatChanges {
-                appendChangeLongEntry()
-            }
-            appendLine()
-        }
-
-        // chores and other known changes
-        groupBySorted({ constants.headerTexts[it.scope] ?: constants.headerTexts[it.type] }) {
-            appendLine(groupKey)
-            with({ constants.headerTexts.containsKey(it.scope) }) {
-                formatChanges {
-                    appendChangeLongEntry()
-                }
-            }
-            formatChanges {
-                appendChangeLongEntry()
-            }
-            appendLine()
-        }
-
-        // other unknown changes
-        otherwise {
-            appendLine(constants.otherChange)
-            formatChanges {
-                appendChangeLongEntry()
-            }
-            appendLine()
-        }
-
-        appendLine(constants.footer)
-    }
+gitHubRelease {
+    projectName.set("OS conditions Spring Boot starter.")
+    projectDescription.set("This is a Spring Boot Starter for OS detection.")
+    developer.set("bitfist")
+    licenseFile.set(projectDir.resolve("LICENSE.txt"))
+    license.set("The Apache License, Version 2.0")
+    licenseUri.set(URI("https://www.apache.org/licenses/LICENSE-2.0"))
 }
-
-// look up git tag
-val gitTagVersion: String = ByteArrayOutputStream().use { outputStream ->
-    project.exec {
-        commandLine("git", "tag", "--points-at", "HEAD")
-        standardOutput = outputStream
-    }
-    outputStream.toString().trim()
-}
-
-val semanticVersionRegex = "\\d+\\.\\d+\\.\\d+".toRegex()
-
-version = if (gitTagVersion.matches(semanticVersionRegex)) {
-    gitTagVersion // version provided by git tag
-} else {
-    semver.version // version provided by the semantic versioning plugin
-}
-//endregion
-
-//region Publishing
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            groupId = project.group as String
-            artifactId = project.name
-            version = project.version as String
-
-            pom {
-                name.set("Starter for OS detection.")
-                description.set("This is a Spring Boot Starter for OS detection.")
-                url.set("https://github.com/bitfist/os-conditions-spring-boot-starter")
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("bitfist")
-                        name.set("bitfist")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:git://github.com/bitfist/os-conditions-spring-boot-starter.git")
-                    developerConnection.set("scm:git:ssh://github.com:bitfist/os-conditions-spring-boot-starter.git")
-                    url.set("https://github.com/bitfist/os-conditions-spring-boot-starter")
-                }
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            name = "OSSRH"
-            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = System.getenv("MAVEN_USERNAME")
-                password = System.getenv("MAVEN_PASSWORD")
-            }
-        }
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/bitfist/os-conditions-spring-boot-starter")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-}
-//endregion
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,27 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenCentral()
+
+        maven {
+            name = "gradle-github-support"
+            url = uri("https://maven.pkg.github.com/bitfist/gradle-github-support")
+            credentials {
+                try {
+                    username = settings.extra["GPR_USER"] as String?
+                } catch (exception: ExtraPropertiesExtension.UnknownPropertyException) {
+                    username = System.getenv("GITHUB_ACTOR") ?: throw IllegalArgumentException("GITHUB_ACTOR not set")
+                }
+                try {
+                    password = settings.extra["GPR_KEY"] as String?
+                } catch (exception: ExtraPropertiesExtension.UnknownPropertyException) {
+                    password = System.getenv("GITHUB_TOKEN") ?: throw IllegalArgumentException("GITHUB_TOKEN not set")
+                }
+            }
+        }
+    }
+    plugins {
+        id("io.github.bitfist.gradle-github-support.release") version "0.2.2"
     }
 }
 
-val name: String by settings
-rootProject.name = name
+rootProject.name = "os-conditions-spring-boot-starter"
