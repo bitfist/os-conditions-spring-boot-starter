4fcd32d826d9b238d1f4815f10aeda31c8e9d329
bitfist <me@renelebherz.com>
2025-07-19 02:18:07 +0200

    fix: tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     // Dependencies
     implementation("org.springframework.boot:spring-boot-autoconfigure")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 //endregion
 
