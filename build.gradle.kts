f09fc8b8fd042cc5c8e28055e4aef7162f3f7124
bitfist <me@renelebherz.com>
2025-11-20 15:22:23 +0000

    chore(deps): Update dependency org.springframework.boot:spring-boot-dependencies to v3.5.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 
 dependencies {
     // BOMs
-    implementation(platform("org.springframework.boot:spring-boot-dependencies:3.5.7"))
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:3.5.8"))
 
     // Dependencies
     implementation("org.springframework.boot:spring-boot-autoconfigure")
