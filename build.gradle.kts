5e6ad3d7a3cc8dc130ff7e9e677fe2725d770efa
René Lebherz <me@renelebherz.com>
2026-08-20 20:42:30 +0200

    chore(deps): Update dependency org.springframework.boot:spring-boot-dependencies to v4.1.1 (#62)

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 
 dependencies {
     // BOMs
-    implementation(platform("org.springframework.boot:spring-boot-dependencies:4.1.0"))
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:4.1.1"))
 
     // Dependencies
     implementation("org.springframework.boot:spring-boot-autoconfigure")
